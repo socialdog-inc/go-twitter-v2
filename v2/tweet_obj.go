5adaa9a6ef5a93edf474e3a01111c1a06d416269
uuta <nightrage.metal66@gmail.com>
2024-02-01 18:04:27 +0900

    Add TweetNoteTweetObj to TweetObj

--- v2/tweet_obj.go
+++ v2/tweet_obj.go
@@ -73,6 +73,7 @@ type TweetObj struct {
 	ReferencedTweets   []*TweetReferencedTweetObj   `json:"referenced_tweets,omitempty"`
 	Source             string                       `json:"source,omitempty"`
 	WithHeld           *WithHeldObj                 `json:"withheld,omitempty"`
+	NoteTweet          *TweetNoteTweetObj           `json:"note_tweet,omitempty"`
 }
 
 // TweetAttachmentsObj specifics the type of attachment present in the tweet
@@ -122,4 +123,10 @@ type TweetMetricsObj struct {
 type TweetReferencedTweetObj struct {
 	Type string `json:"type"`
 	ID   string `json:"id"`
+}
+
+// TweetNoteTweetObj is information for Tweets longer than 280 characters.
+type TweetNoteTweetObj struct {
+	Text     string              `json:"text"`
+	Entities *EntityNoteTweetObj `json:"entities"`
 }
